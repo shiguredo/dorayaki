788cce5bfcd172aea06aaafebc49a3d5c168567d
NAKAI Ryosuke <nakai@shiguredo.jp>
2015-10-31 23:56:09 +0900

    mv dora_datetime test

--- include/dora_datetime.hrl
+++ include/dora_datetime.hrl
@@ -1,3 +1,18 @@
 -define(DORA_TZ_UTC, utc).
 -define(DORA_TZ_JST, jst).
 -define(DORA_TZ_ECT, ect).
+
+
+-record(dora_timestamp, {
+          year :: non_neg_integer(),
+          month :: dora_datetime:month(),
+          day :: dora_datetime:day(),
+          hour :: 0..23,
+          minute :: 0..59,
+          second :: 0..59,
+          micro_second :: 0..999999,
+          tz_offset = 0 :: non_neg_integer(),
+          tz_designator = <<"Z">> :: binary()
+         }).
+
+
